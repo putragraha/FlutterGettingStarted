ecb6af0a681125d8643f728301844b24cd83ce62
nugrahap <putra.nugraha@dana.id>
2020-04-19 00:27:03 +0700

    Apply MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Center(
-      child: Text(
-      "Hello Flutter",
-      textDirection: TextDirection.ltr
-      )
+  runApp(MaterialApp(
+    title: "Hello Flutter App",
+    home: Material(
+      color: Colors.deepPurple,
+      child: Center(child: Text("Hello Flutter", textDirection: TextDirection.ltr))
     )
-  );
+  ));
 }
