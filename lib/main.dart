987b07771d06986c222e7781ce30f43107eb6038
nugrahap <putra.nugraha@dana.id>
2020-04-19 00:58:02 +0700

    Implement Fat Arrow in main()

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(HelloFlutterApp());
-}
+void main() => runApp(HelloFlutterApp());
 
 class HelloFlutterApp extends StatelessWidget {
 
