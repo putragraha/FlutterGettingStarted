2cb659ec42a445bff678ed87506812fdab5db13f
nugrahap <putra.nugraha@dana.id>
2020-04-19 01:18:00 +0700

    Add new method and condition logic

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -8,7 +8,7 @@ class Home extends StatelessWidget {
               color: Colors.deepPurple,
               child: Center(
                   child: Text(
-                    "Hello Flutter",
+                    getGreetMessage(),
                     textDirection: TextDirection.ltr,
                     style: TextStyle(
                       color: Colors.white, 
@@ -17,5 +17,17 @@ class Home extends StatelessWidget {
                   )
               )
             );
+  }
+
+  String getGreetMessage() {
+    num currentHour = DateTime.now().hour;
+
+    if (currentHour < 12) {
+      return "Good Morning";
+    } else if (currentHour < 18) {
+      return "Good Afternoon";
+    } else {
+      return "Good Night";
+    }
   }
 }
