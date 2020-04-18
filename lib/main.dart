6ce785283c892ac388f2d48c9bdca43e0ade9898
nugrahap <putra.nugraha@dana.id>
2020-04-19 00:36:49 +0700

    Implement TextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Hello Flutter App",
-    home: Material(
-      color: Colors.deepPurple,
-      child: Center(child: Text("Hello Flutter", textDirection: TextDirection.ltr))
-    )
-  ));
+      title: "Hello Flutter App",
+      home: Material(
+          color: Colors.deepPurple,
+          child: Center(
+              child: Text("Hello Flutter",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(color: Colors.white, fontSize: 36.0))))));
 }
