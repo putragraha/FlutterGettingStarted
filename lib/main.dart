06692a3b040c520cb37353cc9b530c9c3aef95ba
nugrahap <putra.nugraha@dana.id>
2020-04-19 00:53:26 +0700

    Reformat code

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,25 @@ void main() {
   runApp(MaterialApp(
       title: "Hello Flutter App",
       home: Scaffold(
-          appBar: AppBar(title: Text("Title in App Bar")),
+          appBar: AppBar(
+            title: Text(
+              "Title in App Bar"
+            )
+          ),
           body: Material(
               color: Colors.deepPurple,
               child: Center(
-                  child: Text("Hello Flutter",
-                      textDirection: TextDirection.ltr,
-                      style:
-                          TextStyle(color: Colors.white, fontSize: 36.0)))))));
+                  child: Text(
+                    "Hello Flutter",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      color: Colors.white, 
+                      fontSize: 36.0
+                    )
+                  )
+                )
+              )
+          )
+        )
+    );
 }
