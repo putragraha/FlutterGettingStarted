41e19ce87a3cb7f3bc4e51004badfb533b7510d4
nugrahap <putra.nugraha@dana.id>
2020-04-19 08:44:36 +0700

    Implement margin

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -8,8 +8,7 @@ class Home extends StatelessWidget {
       child: Container(
         alignment: Alignment.center,
         color: Colors.deepOrangeAccent,
-        width: 192.0,
-        height: 96.0,
+        margin: EdgeInsets.all(20.0),
         child: Text("Pizza", textDirection: TextDirection.ltr)
     ));
   }
