bf9e81f308f576079505f6eb65705c8642634133
nugrahap <putra.nugraha@dana.id>
2020-04-19 08:23:56 +0700

    Implement container

---
+++ lib/ui/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      color: Colors.deepOrangeAccent,
+      child: Text("Pizza", textDirection: TextDirection.ltr)
+    );
+  }
+}
